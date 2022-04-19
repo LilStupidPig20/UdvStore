2f6a27a7d7e13d5b16e204eb8f0beb29ed91c7c8
KPEesti <iwantoseeyou1@gmail.com>
2022-04-20 02:03:16 +0500

    Rules page added
    I have added a rules page. It is available at "/rules".
    Nothing else hehe

--- UdvStore/ClientApp/src/routes.js
+++ UdvStore/ClientApp/src/routes.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
+import RulesPage from './pages/RulesPage/RulesPage';
 
 
 export const useRoutes = (isAuthenticated) => {
-    if(isAuthenticated) {
+    if (isAuthenticated) {
         return (
-        <Switch>
-            <Route path="/profile" exact>
-                <div>ты крутой</div>
-            </Route>
-            <Redirect to="/profile"/>
-        </Switch>
+            <Switch>
+                <Route path="/profile" exact>
+                    <div>ты крутой</div>
+                </Route>
+                <Route path="/rules" >
+                    <RulesPage />
+                </Route>
+                <Redirect to="/profile" />
+            </Switch>
         );
     }
     return (
