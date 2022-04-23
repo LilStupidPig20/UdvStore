fdbae42b28e37ea2fba5ef86938eea2bfac8bcbb
fateful1 <facelessasha@yandex.ru>
2022-04-23 20:36:59 +0500

    обновлен путь логина

--- UdvStore/ClientApp/src/pages/AuthPage/index.js
+++ UdvStore/ClientApp/src/pages/AuthPage/index.js
@@ -17,7 +17,7 @@ export const AuthPage = () => {
 
     const loginHandler = async () => {
         try {
-          const data = await request("/api/Auth/Login", "POST", { ...form });
+          const data = await request("/Login/Authentication", "POST", { ...form });
             auth.login(data.token, data.userId, data.fullName);
         } catch (error) {
           setFail(true);
