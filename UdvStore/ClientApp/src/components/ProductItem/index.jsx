f439270601579b4855b6ca8b0b22fe47850a4b6c
KPEesti <iwantoseeyou1@gmail.com>
2022-04-26 20:09:06 +0500

    Добавил отдельную карточку товара Работает криво, но работет, хых

--- UdvStore/ClientApp/src/components/ProductItem/index.jsx
+++ UdvStore/ClientApp/src/components/ProductItem/index.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import styles from "./product.module.css"
+import styles from "./product.module.css";
+import { Link } from "react-router-dom";
 
 export default function ProductItem({
+    url,
     imgSrc = "/imgs/ProductImages/Кружка.JPG",
     title = "Кружка UDV с надписью “Удиви мир”",
-    price = 15
+    price = 15,
+    id = 1
 }) {
 
     return (
-        <div className={styles.product}>
-            <img src={imgSrc} alt="" className={styles.productImg} />
-            <div className={styles.title}>{title}</div>
-            <div className={styles.price}>Цена: {price} UC</div>
-            <button className={styles.addButton}>В корзину</button>
-        </div>
+        <Link to={`/product/${id}`} style={{ textDecoration: 'none' }}>
+            <div className={styles.product}>
+                <img src={imgSrc} alt="" className={styles.productImg} />
+                <div className={styles.title}>{title}</div>
+                <div className={styles.price}>Цена: {price} UC</div>
+                <button className={styles.addButton}>В корзину</button>
+            </div>
+        </Link>
     );
 }
--- UdvStore/ClientApp/src/routes.js
+++ UdvStore/ClientApp/src/routes.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import RulesPage from './pages/RulesPage';
 import ProfilePage from './pages/ProfilePage';
 import StorePage from './pages/StorePage';
+import ProductPage from './pages/ProductPage';
 import MainLayout from './components/layouts/MainLayout';
 import NonAuthLayout from './components/layouts/NonAuthLayout';
 import { AuthPage } from './pages/AuthPage';
@@ -10,6 +11,14 @@ import { SendFormPage } from './pages/SendFormPage';
 
 
 export const useRoutes = (isAuthenticated) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://localhost:5001/store/getAll')
+            .then(res => res.json())
+            .then(items => setProducts(items))
+    }, []);
+    
     if (isAuthenticated) {
         return (
             <MainLayout>
@@ -21,8 +30,15 @@ export const useRoutes = (isAuthenticated) => {
                         <RulesPage />
                     </Route>
                     <Route path="/store" >
-                        <StorePage />
+                        <StorePage products={products} />
                     </Route>
+                    {
+                        products.map(product => {
+                            return <Route path={`/product/${product.id}`} >
+                                <ProductPage product={product}/>
+                            </Route>
+                        })
+                    }
                     <Route path="/sendForm">
                         <SendFormPage />
                     </Route>
@@ -41,7 +57,7 @@ export const useRoutes = (isAuthenticated) => {
                     <RulesPage />
                 </Route>
                 <Route path="/store" >
-                    <StorePage />
+                    <StorePage  products={products} />
                 </Route>
                 <Redirect to="/" />
             </Switch>
