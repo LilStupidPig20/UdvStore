ee75744bd1965165db3633aa16899c7231eeb3e7
avolgarev <avolgarev@ussc.ru>
2022-05-18 21:17:25 +0500

    Fix AdminAccrual

--- UdvStore/RequestModels/AccrualToUsersModel.cs
+++ UdvStore/RequestModels/AccrualToUsersModel.cs
@@ -5,6 +5,7 @@ namespace UdvStore.RequestModels
 {
     public class AccrualToUsersModel
     {
+        public string NameOfEvent { get; set; }
         public string Description { get; set; }
         public decimal Coins { get; set; } 
         public DateTime DateOfEvent { get; set; }
