c1b3c2b68aa62b21ac4db1b063347a658799ec08
KPEesti <iwantoseeyou1@gmail.com>
2022-05-30 23:01:56 +0500

    ну тепрь точно всё нормально должно отображаться

--- UdvStore/ClientApp/src/components/UserActivityItem/index.jsx
+++ UdvStore/ClientApp/src/components/UserActivityItem/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './activity.module.css';
 
 export default function UserActivityItem({
@@ -17,6 +17,7 @@ export default function UserActivityItem({
     return (
         <div onClick={() => setFlag(!flag)}>
             <div className={`${styles.container} ${comment !== null && styles.pointer}`}>
+                <div className={styles.columns}>
                     <div className={styles.titleItem}>{title}</div>
                     <div className={styles.dateItem}>{date}</div>
                     {
@@ -30,6 +31,7 @@ export default function UserActivityItem({
                             :
                             <div className={`${styles.coinsItem} ${styles.grey}`}>{coinsAsString}</div>
                     }
+                </div>
                 {
                     flag && comment !== null
                         ?
